f809435e349434153797f37feb99e396d022e353
LivyR <ORodriguez@student.neumont.edu>
2020-11-22 11:05:49 -0700

    changes

--- controller.js
+++ controller.js
@@ -13,3 +13,72 @@ entrypoint.innerHTML =
     </div>
 </div>`;
 })
+
+
+
+var total = 0;
+addTopping(topping); {
+  This.total += topping.cost;
+  document.getElementByID("total").innerHTML = this.total;
+}
+
+if (topping >= 2 < 5 || topping.extra){
+    total + 1;
+    console.log(this.total)
+}
+ else if (topping = 5) {
+     total + 3;
+     console.log(this.total)
+ }
+ else if (topping > 5) {
+     total + 4
+     console.log("Special Deal " + this.total)
+     
+ }
+ else {
+     console.log(this.total)
+ }
+
+
+
+function size1(){
+     var checkbox = document.getElementById("XL");
+     var text = document.getElementById("text1");
+
+     if (checkbox.checked == true){
+         text.style.display = "block";
+     } else {
+         text.style.display = "none";
+     }
+    }
+     function size2(){
+        var checkbox = document.getElementById("Large");
+        var text = document.getElementById("text2");
+   
+        if (checkbox.checked == true){
+            text.style.display = "block";
+        } else {
+            text.style.display = "none";
+        }
+    }
+    function size3(){
+        var checkbox = document.getElementById("Medium");
+        var text = document.getElementById("text3");
+   
+        if (checkbox.checked == true){
+            text.style.display = "block";
+        } else {
+            text.style.display = "none";
+        }
+    }
+    function size4(){
+            var checkbox = document.getElementById("Small");
+            var text = document.getElementById("text4");
+       
+            if (checkbox.checked == true){
+                text.style.display = "block";
+            } else {
+                text.style.display = "none";
+            }
+        
+ }
